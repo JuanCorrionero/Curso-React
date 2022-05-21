b2d5c1d80e8b92cd8a95c7d944dcb6c084e9384a
Juani <juanignacio.corrionero@gmail.com>
2022-05-21 19:12:38 -0300

    Class 37

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PrimeraApp = () => {
+
+    return <>
+        <h1>Hola Mundo</h1>
+        <p>Mi primera aplicación</p>
+    </>
+    
+
+
+
+}
+
+
+export default PrimeraApp;
